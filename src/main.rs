4dbc485b7f7b9f6cc8955cbb54b5c6133645b0b0
micah <106090036+itsmicah722@users.noreply.github.com>
2025-07-17 20:06:33 -0400

    Added Vulkan instance creation and macOS portability extension

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![deny(unsafe_op_in_unsafe_fn)]
 #![allow(
     dead_code,
     unused_variables,
@@ -5,11 +6,20 @@
     clippy::unnecessary_wraps
 )]
 
+use std::ffi::CStr;
+
 // Here we use `Result<T, anyhow::Error>` aka - `Result<()>` for easier error handling and
-// propagation, since `anyhow` wraps those types and can store any error. Propagation can be done
-// via "?", allowing us to avoid more matches or unwraps.
-use anyhow::Result;
-use log::trace;
+// propagation, since anyhow wraps those types and can store any error that implements
+// `std::error`. Propagation can be done via "?", allowing us to avoid more matches or unwraps.
+use anyhow::{anyhow, Context, Result};
+use log::*;
+use vulkanalia::{
+    loader::{LibloadingLoader, LIBRARY}, vk, vk::{HasBuilder, InstanceV1_0},
+    window as vk_window,
+    Entry,
+    Instance,
+    Version,
+};
 use winit::{
     dpi::LogicalSize,
     event::{Event, WindowEvent},
@@ -17,19 +27,29 @@ use winit::{
     window::{Window, WindowBuilder},
 };
 
+const PORTABILITY_MACOS_VERSION: Version = Version::new(1, 3, 216);
+
 fn main() -> Result<()> {
+    unsafe {
+        std::env::set_var("RUST_LOG", "trace");
+    }
     pretty_env_logger::init();
 
+    // Create the event loop.
+    let event_loop = EventLoop::new().context("Failed to create event loop.")?;
+
     // Create the window.
-    let event_loop = EventLoop::new()?;
     let window = WindowBuilder::new()
         .with_title("Vulkan App")
         .with_inner_size(LogicalSize::new(600, 400))
         .with_active(false)
-        .build(&event_loop)?;
+        .build(&event_loop)
+        .context("Failed to create window.")?;
 
     // Create the Vulkan app
-    let mut app = unsafe { App::create(&window)? };
+    let mut app = unsafe { App::create(&window).context("Failed to create Vulkan App.")? };
+
+    // Run the event loop
     event_loop.run(move |event, window_target| {
         match event {
             // Request a redraw when all events have been processed (so we can render continuously
@@ -41,7 +61,6 @@ fn main() -> Result<()> {
                 // Render a frame if the App is not being destroyed.
                 WindowEvent::RedrawRequested if !window_target.exiting() => {
                     unsafe { app.render(&window) }.unwrap();
-                    trace!("Vulkan app requested a redraw.")
                 }
 
                 // Destroy the App.
@@ -50,10 +69,11 @@ fn main() -> Result<()> {
                     unsafe {
                         app.destroy();
                     }
-                    trace!("Vulkan App was destroyed.")
                 }
+
                 _ => {}
             },
+
             _ => {}
         }
     })?;
@@ -63,12 +83,33 @@ fn main() -> Result<()> {
 
 /// Main `App` which implements Vulkan boilerplate functionality.
 #[derive(Clone, Debug)]
-struct App {}
+struct App {
+    entry: Entry,
+    instance: Instance,
+}
 
 impl App {
     /// Creates the Vulkan App.
     unsafe fn create(window: &Window) -> Result<Self> {
-        Ok(Self {})
+        let (entry, instance) = unsafe {
+            let loader = LibloadingLoader::new(LIBRARY)?;
+            let entry = Entry::new(loader).map_err(|b| anyhow!("{}", b))?;
+            let instance = match create_instance(window, &entry) {
+                Ok(instance) => {
+                    trace!("Created Vulkan instance.");
+                    instance
+                }
+
+                Err(e) => {
+                    error!("Failed to create Vulkan instance: {:?}", e);
+                    return Err(e);
+                }
+            };
+
+            (entry, instance)
+        };
+
+        Ok(Self { entry, instance })
     }
 
     /// Renders a frame for our Vulkan App.
@@ -77,9 +118,54 @@ impl App {
     }
 
     /// Destroys the Vulkan app.
-    unsafe fn destroy(&mut self) {}
+    unsafe fn destroy(&mut self) {
+        unsafe {
+            self.instance.destroy_instance(None);
+            trace!("Destroyed Vulkan instance.")
+        }
+    }
 }
 
 /// All Vulkan handles and associated properties used by `App`.
 #[derive(Clone, Debug, Default)]
 struct AppData {}
+
+unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance> {
+    let application_info = vk::ApplicationInfo::builder()
+        .application_name(c"Vulkan App".to_bytes_with_nul())
+        .application_version(vk::make_version(1, 0, 0))
+        .engine_name(c"No Engine".to_bytes_with_nul())
+        .engine_version(vk::make_version(1, 0, 0))
+        .api_version(vk::make_version(1, 0, 0));
+
+    // `ext` is &&ExtensionNames, which is a reference to a list of references to static C-String
+    // extension names.
+    let mut extensions = Vec::new();
+    for ext in vk_window::get_required_instance_extensions(window) {
+        let ptr = ext.as_ptr();
+        let name = unsafe { CStr::from_ptr(ptr) }.to_string_lossy();
+        debug!("Adding extension: {}", name);
+        extensions.push(ptr);
+    }
+
+    // Required by Vulkan SDK on macOS since 1.3.216.
+    let flags = if cfg!(target_os = "macos") && entry.version()? >= PORTABILITY_MACOS_VERSION {
+        trace!("Enabling extensions for macOS portability.");
+        extensions.push(
+            vk::KHR_GET_PHYSICAL_DEVICE_PROPERTIES2_EXTENSION
+                .name
+                .as_ptr(),
+        );
+        extensions.push(vk::KHR_PORTABILITY_ENUMERATION_EXTENSION.name.as_ptr());
+        vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
+    } else {
+        vk::InstanceCreateFlags::empty()
+    };
+
+    let create_info = vk::InstanceCreateInfo::builder()
+        .application_info(&application_info)
+        .enabled_extension_names(&extensions)
+        .flags(flags);
+
+    unsafe { Ok(entry.create_instance(&create_info, None)?) }
+}
